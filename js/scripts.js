da4c4ee752c45ca2ac9b02fe92c5ab585a43732b
Axel <kurtastraube@gmail.com>
2015-06-05 14:20:54 -0700

    Update pricing function and pass corresponding spec.

--- js/scripts.js
+++ js/scripts.js
@@ -10,42 +10,74 @@ Pizza.prototype.price = function() {
   if (this.pizzaSize === "medium") {
     standardPrice += 2;
   } else if (this.pizzaSize === "large") {
-    standardPrice =+ 4;
+    standardPrice += 4;
   }
 
-  if (this.toppings.length === 1) {
-    standardPrice += .25;
-  } else if (this.toppings.length === 2) {
-    standardPrice += .50;
-  } else if (this.toppings.length === 3) {
-    standardPrice += .75;
-  } else if (this.toppings.length === 4) {
-    standardPrice += 1;
-  } else if (this.toppings.length === 5) {
-    standardPrice += 1.25;
-  } else if (this.toppings.length === 6) {
-    standardPrice += 1.5;
-  } else if (this.toppings.length === 7) {
-    standardPrice += 1.75;
-  } else if (this.toppings.length === 8) {
-    standardPrice += 2;
-  } else if (this.toppings.length === 9) {
-    standardPrice += 2.25;
-  } else if (this.toppings.length === 10) {
-    standardPrice += 2.5;
+  // if (this.toppings.length != 0) {
+    // if (this.toppings.length === 1) {
+    //   standardPrice += .25;
+    // } else if (this.toppings.length === 2) {
+    //   standardPrice += .50;
+    // } else if (this.toppings.length === 3) {
+    //   standardPrice += .75;
+    // } else if (this.toppings.length === 4) {
+    //   standardPrice += 1;
+    // } else if (this.toppings.length === 5) {
+    //   standardPrice += 1.25;
+    // } else if (this.toppings.length === 6) {
+    //   standardPrice += 1.5;
+    // } else if (this.toppings.length === 7) {
+    //   standardPrice += 1.75;
+    // } else if (this.toppings.length === 8) {
+    //   standardPrice += 2;
+    // } else if (this.toppings.length === 9) {
+    //   standardPrice += 2.25;
+    // } else if (this.toppings.length === 10) {
+    //   standardPrice += 2.5;
+    // }
+  // }
+
+  if (this.quantity === 2) {
+    standardPrice *= 2;
+  } else if (this.quantity === 3) {
+    standardPrice *= 3;
+  } else if (this.quantity === 4) {
+    standardPrice *= 4;
+  } else if (this.quantity === 5) {
+    standardPrice *= 5;
+  } else if (this.quantity === 6) {
+    standardPrice *= 6;
+  } else if (this.quantity === 7) {
+    standardPrice *= 7;
+  } else if (this.quantity === 8) {
+    standardPrice *= 8;
+  } else if (this.quantity === 9) {
+    standardPrice *= 9;
+  } else if (this.quantity === 10) {
+    standardPrice *= 10;
   }
 
   return standardPrice;
 };
 
-$(function() {
+function updateTextOnPage(selector, text) {
+  $(selector).text(text);
+}
+
+$(document).ready(function(){
   $(".cart").hide();
 
   $("form#create-pizza").submit(function(event) {
     event.preventDefault();
 
-    newPizza = new Pizza();
+    var newPizzaSize = $("#pizza-size :selected").val();
+    var newPizzaQuantity = $("#quantity :selected").val();
 
+    newPizza = new Pizza(newPizzaSize, newPizzaQuantity);
 
+    updateTextOnPage("#pizza-price", newPizza.price());
+
+    $(".create-pizza").slideUp();
+    $(".cart").show();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,9 @@ describe('Pizza', function() {
     expect(testPizza.pizzaSize).to.equal("large");
     expect(testPizza.quantity).to.equal(1);
   });
+
+  it("returns the price of a pizza", function() {
+    var testPizza = new Pizza("large", 2);
+    expect(testPizza.price()).to.equal(22);
+  });
 });
